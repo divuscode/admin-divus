5006d834a9844d1f8d20a79d63323be5c9b18b61
divuscodeDjordje <djordjesunjevaric87@gmail.com>
2014-05-17 16:28:19 +0200

    created  edit icon for menu links

---
+++ pages-edit-icon.php
@@ -0,0 +1,11 @@
+<span class="pages-edit-icon">     
+    <a href="add-page.php" class="add-icon" data-toggle="tooltip" data-placement="top" title="Add new page">
+        <i class="fa fa-plus"></i>
+    </a>
+    <a href="edit-page.php" class="edit-icon" data-toggle="tooltip" data-placement="top" title="Edit page">
+        <i class="fa fa-edit"></i>
+    </a>
+    <a href="#" class="delete-icon" data-toggle="tooltip" data-placement="top" title="Delete page">
+        <i class="fa fa-eraser"></i>
+    </a>
+</span>
