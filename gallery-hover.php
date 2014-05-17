14b27a36f6e0a84b48eab69699540acbf0a0117b
divuscodeDjordje <djordjesunjevaric87@gmail.com>
2014-05-17 18:28:47 +0200

    gallery hover

---
+++ gallery-hover.php
@@ -0,0 +1,6 @@
+<div class="gallery-hover">
+    <a href="" data-toggle="tooltip" data-placement="top" title="Edit image"><i class="fa fa-edit"></i>
+    </a>
+    <a href="" data-toggle="tooltip" data-placement="top" title="Delete image"><i class="fa fa-eraser"></i>
+    </a>
+</div>
