4680d2d9e7d8f55b8621043aaa8317d15d685a54
divuscodeDjordje <djordjesunjevaric87@gmail.com>
2014-05-17 16:27:14 +0200

    created inbox category for messages page

---
+++ inbox-category.php
@@ -0,0 +1,39 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <a href="" class="btn btn-admin-divus-dark">
+            Write message
+        </a>
+    </div>
+    <div class="panel-body inbox-category">
+        <ul class="list-group">
+            <li class="list-group-item active"><a href="">
+                <i class="fa fa-inbox"></i>
+                <span>Inbox</span>
+                <span class="badge badge-info pull-right">6</span>
+            </a></li>
+            <li class="list-group-item"><a href="">
+                <i class="fa fa-envelope-o"></i>
+                <span>Send mail</span>
+            </a></li>
+            <li class="list-group-item"><a href="">
+                <i class="fa fa-star"></i>
+                <span>Starred</span>
+                <span class="badge badge-danger pull-right">3</span>
+            </a></li>
+            <li class="list-group-item"><a href="">
+                <i class="fa fa-comment-o"></i>
+                <span>Chat</span>
+                <span class="badge badge-success pull-right">9</span>
+            </a></li>
+            <li class="list-group-item"><a href="">
+                <i class="fa fa-pencil"></i>
+                <span>Draft</span>
+                <span class="badge badge-warning pull-right">1</span>
+            </a></li>
+            <li class="list-group-item"><a href="">
+                <i class="fa fa-trash-o"></i>
+                <span>Spam</span>
+            </a></li>
+        </ul>
+    </div>
+</div>
